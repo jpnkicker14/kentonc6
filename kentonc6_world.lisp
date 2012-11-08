40cf2a313b99d98005cf6922139a44032568cacf
Kenton Chun <chunkenton1@gmail.com>
2012-11-08 00:05:40 -1000

    kentonc6_world.lisp

---
+++ kentonc6_world.lisp
@@ -0,0 +1,16 @@
+;planets                                                                        
+(add-location bluefish you have successfully landed on planet bluefish the plan\
+et with the purple fish.)
+(add-location crystalpineapple you have just landed on planet crystalpineapple \
+planet of the many cats.)
+(add-location octopusdisco you have just landed on planet octopusdisco the plan\
+et of the dancing koalas.)
+(add-location sharpblizzard you have just crash landed on planet sharpblizzard \
+the planet with the heat storms. there is no visible way out.)
+(add-location hotpapaya you have just landed on planet hot papaya the planet fa\
+mous for its delicious but poisonous bananas.)
+(add-location newdinosaur you have just landed on planet newdinosaur the planet\
+ of lizards.)
+
+;paths                                                                          
+;(add-path lightstream space-station bluefish forward)

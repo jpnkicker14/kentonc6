7f58fbce89f2160e366587a0fc1cfe6505f565a1
amymalia <amaliat21@gmail.com>
2012-11-08 19:12:09 -1000

    added all location paths

--- kentonc6_world.lisp
+++ kentonc6_world.lisp
@@ -1,16 +1,15 @@
-;planets                                                                        
-(add-location bluefish you have successfully landed on planet bluefish the plan\
-et with the purple fish.)
-(add-location crystalpineapple you have just landed on planet crystalpineapple \
-planet of the many cats.)
-(add-location octopusdisco you have just landed on planet octopusdisco the plan\
-et of the dancing koalas.)
-(add-location sharpblizzard you have just crash landed on planet sharpblizzard \
-the planet with the heat storms. there is no visible way out.)
-(add-location hotpapaya you have just landed on planet hot papaya the planet fa\
-mous for its delicious but poisonous bananas.)
-(add-location newdinosaur you have just landed on planet newdinosaur the planet\
- of lizards.)
+;planets
+(add-location bluefish you have successfully landed on planet bluefish the planet with the purple fish.)
+(add-location p1 you have just landed on planet crystalpineapple planet of the many cats.)
+(add-location p2 you have just landed on planet octopusdisco the planet of the dancing koalas.)
+(add-location pcrash you have just crash landed on planet sharpblizzard the planet with the heat storms. there is no visible way out.)
+(add-location p4 you have just landed on planet hot papaya the planet famous for its delicious but poisonous bananas.)
+(add-location p5 you have just landed on planet newdinosaur the planet of lizards.) 
 
-;paths                                                                          
-;(add-path lightstream space-station bluefish forward)
+;paths
+(add-path space-station bluefish forward)
+(add-path bluefish p1 left right)
+(add-path bluefish p2 right left)
+(add-path bluefish pcrash forward)
+(add-path bluefish p4 up down)
+(add-path bluefish p5 down up)

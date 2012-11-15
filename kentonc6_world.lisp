8cd05cf30a5af99f25bd7387d79d7c385644a593
Kenton Chun <chunkenton1@gmail.com>
2012-11-14 22:51:35 -1000

    added/changed world

--- kentonc6_world.lisp
+++ kentonc6_world.lisp
@@ -1,18 +1,24 @@
 ;planets
-(add-location bluefish you have successfully landed on planet bluefish the planet with the purple fish.)
-(add-location p1 you have just landed on planet crystalpineapple planet of the many cats.)
-(add-location p2 you have just landed on planet octopusdisco the planet of the dancing koalas.)
-(add-location pcrash you have just crash landed on planet sharpblizzard the planet with the heat storms. there is no visible way out.)
-(add-location p4 you have just landed on planet hot papaya the planet famous for its delicious but poisonous bananas.)
-(add-location p5 you have just landed on planet newdinosaur the planet of lizards. There is vast lush and inhabitable land all around you.) 
+(add-location bluefish you have successfully landed on planet bluefish the planet with the purple fish. though you may not be able to eat the fish use this planet as your center if you get lost.)
+(add-location p1 you have just landed on the giving planet.);wood bricks
+(add-location p2 you have just landed on planet octopusdisco. what you see on the ground does not mean there were hoes but you may want it for another garden tool.);shovel
+(add-location pcrash you have just crash landed on planet hollywood. there is no visible way out instead of partying forver the game will reset.);crash
+(add-location p4 you have just landed on the garden of wheat thins. the lizard seems to be pointing at something on the ground.);poison 
+(add-location p5 you have just landed on planet aladdin.);settle 
+(add-location p6 you have just landed on planet fifty-one. ever heard of it?)
+(add-location p7 you have just landed on planet voi dire. does it really matter?)
+(add-location p8 you have just landed on planet mc. what time is it?);get and make hammer
 
 ;paths
 (add-path earth bluefish up)
 (add-path bluefish p1 left right)
 (add-path bluefish p2 right left)
 (add-path bluefish pcrash forward)
-(add-path bluefish p4 up down)
-(add-path bluefish p5 down up)
+(add-path bluefish p4 back forward)
+(add-path bluefish p5 up down)
+(add-path p1 p6 forward back)
+(add-path p5 p7 left right)
+(add-path p7 p8 foward back)
 
 ;objects on earth
 (add-object shovel-head earth)
@@ -30,11 +36,11 @@
 (add-object banana p4)
 (add-object brick-pile p1)
 (add-object wood-pile p1)
-(add-object gold-pole p1)
+(add-object gold-pole p8)
 (add-object steel-pole p2)
 
 ;create game-actions
-(game-action weld gold-pole hammer-head p1
+(game-action weld gold-pole hammer-head p8
              (cond ((not (have 'hammer-head)) '(you do not have a hammer-head.))
                    (*hammer-welded* '(you have already welded a hammer.))
                    (t (setf *hammer-welded* 't)
